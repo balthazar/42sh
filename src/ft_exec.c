73e420aa91da276e1cd4a8a5b255d028fa07ea20
Balthazar GRONON <bgronon@gmail.com>
2014-03-01 19:23:56 +0100

    changes

--- src/ft_exec.c
+++ src/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0000/00/00 00:00:00 by 5tta              #+#    #+#             */
-/*   Updated: 2014/03/01 18:25:40 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/01 19:18:43 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,6 @@
 
 int					ft_exec(t_btree *node)
 {
-	if (ft_detect_builtin(GETT(node, cmd)[0]))
-	{
-		ft_exec_builtin(node);
-		//t[ft_get_type()](node);
-	}
-	else
-		execve(C(node)->path, C(node)->cmd, C(node)->env);
+	execve(C(node)->path, C(node)->cmd, C(node)->env);
 	return (0);
 }
--- src/ft_fork_and_exec.c
+++ src/ft_fork_and_exec.c
@@ -6,7 +6,7 @@
 /*   By: fbeck <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 14:24:03 by fbeck             #+#    #+#             */
-/*   Updated: 2014/03/01 18:25:59 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/01 19:19:00 by bgronon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,9 @@ void				ft_fork_and_exec(t_btree *node)
 {
 	pid_t			father;
 
-	if (ft_detect_builtin(GETT(node, cmd)[0]))
+	if (-1 == (father = fork()))
+		ft_error("fork failed");
+	else if (!father)
 		ft_exec(node);
-	else
-	{
-		if (-1 == (father = fork()))
-			ft_error("fork failed");
-		else if (!father)
-			ft_exec(node);
-		wait(NULL);
-	}
+	wait(NULL);
 }
