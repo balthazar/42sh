cefc581c7a0554c2a6103646c4e4960caf7c4e96
janteuni <janteuni@student.42.fr>
2014-03-04 17:00:38 +0100

    enleve bullshit

--- src/ft_fork_and_exec.c
+++ src/ft_fork_and_exec.c
@@ -6,7 +6,7 @@
 /*   By: fbeck <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 14:24:03 by fbeck             #+#    #+#             */
-/*   Updated: 2014/03/04 13:07:21 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/03/04 17:00:18 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "42sh.h"
-
-static void				st_add_bgjobs(pid_t father)
-{
-	t_jobs			j;
-	int				nb_tmp;
-	t_list			*list;
-
-	list = CTX->jobs;
-	nb_tmp = 0;
-	j.pid = father;
-	if (!CTX->jobs)
-		j.first = FIRST;
-	else if (!CTX->jobs->next)
-		j.first = SECOND;
-	else
-		j.first = LOOSER;
-	while (list)
-	{
-		if (nb_tmp < ((t_jobs *)list->content)->nb)
-			nb_tmp = ((t_jobs *)list->content)->nb;
-		list = list->next;
-	}
-	j.nb = nb_tmp + 1;
-	ft_lstadd(&CTX->jobs, ft_lstnew(&j, sizeof(t_jobs)));
-}
 
 void				ft_fork_and_exec(t_btree *node)
 {
@@ -53,7 +28,6 @@ void				ft_fork_and_exec(t_btree *node)
 			ft_error("signal error");
 		ft_exec(node);
 	}
+	CTX->child = father;
 	waitpid(father, &status, WUNTRACED);
-	if (WIFSTOPPED(status))
-		st_add_bgjobs(father);
 }
