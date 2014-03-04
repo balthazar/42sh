bd4bc45539a55a77649be6021c6717d867529495
mpillet <mpillet@student.42.fr>
2014-03-04 13:11:27 +0100

    Formating

--- src/context.c
+++ src/context.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 15:07:16 by mpillet           #+#    #+#             */
-/*   Updated: 2014/03/04 12:41:11 by bgronon          ###   ########.fr       */
+/*   Updated: 2014/03/04 13:11:16 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,34 @@
 #include <stdlib.h>
 #include "42sh.h"
 
+static void			st_set_defaults(t_ctx *ctx)
+{
+	struct winsize	w;
+
+	ioctl(ft_get_fd(), TIOCGWINSZ, &w);
+	ctx->cols = w.ws_col;
+	ctx->rows = w.ws_row;
+	ctx->env = NULL;
+	ctx->i = 0;
+	ctx->prompt = 0;
+	ctx->len = 0;
+	ctx->history = NULL;
+	ctx->cur_h = NULL;
+	ctx->end_h = NULL;
+	ctx->child = -1;
+	ft_bzero(ctx->line, LINE_LEN);
+	ft_bzero(ctx->buf, BUF_LEN);
+}
+
 t_ctx				*ft_get_ctx(void)
 {
 	static t_ctx	*ctx = NULL;
-	struct winsize	w;
 
 	if (!ctx)
 	{
 		if (!(ctx = (t_ctx *) malloc(sizeof(t_ctx))))
 			ft_error("Can't malloc 'ctx'");
-		ioctl(ft_get_fd(), TIOCGWINSZ, &w);
-		ctx->cols = w.ws_col;
-		ctx->rows = w.ws_row;
-		ctx->env = NULL;
-		ctx->i = 0;
-		ctx->prompt = 0;
-		ctx->len = 0;
-		ctx->history = NULL;
-		ctx->cur_h = NULL;
-		ctx->end_h = NULL;
-		ctx->child = -1;
-		ft_bzero(ctx->line, LINE_LEN);
-		ft_bzero(ctx->buf, BUF_LEN);
+		st_set_defaults(ctx);
 	}
 	return (ctx);
 }
