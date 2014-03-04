b024e8293485c3419eb28e085ceff55b6438f835
mpillet <mpillet@student.42.fr>
2014-03-05 00:02:16 +0100

    update ft_add_char

--- src/ft_add_char.c
+++ src/ft_add_char.c
@@ -6,11 +6,17 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/02 17:04:30 by mpillet           #+#    #+#             */
-/*   Updated: 2014/03/04 23:19:05 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/05 00:00:16 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
+
+static int			st_exceed(t_ctx *ctx)
+{
+	(void) ctx;
+	return (0);
+}
 
 void				ft_add_char(char c)
 {
@@ -28,5 +34,8 @@ void				ft_add_char(char c)
 	++ctx->pos.x;
 	++ctx->i;
 	++ctx->len;
-	ft_putchar(c);
+	if (st_exceed(ctx) > ctx->cols)
+		ft_rewrite();
+	else
+		ft_putchar(c);
 }
