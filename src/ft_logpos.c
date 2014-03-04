11c2adc72d81c9b39f906a377dd568524e7636ef
mpillet <mpillet@student.42.fr>
2014-03-04 23:01:30 +0100

    update ft_logpos

--- src/ft_logpos.c
+++ src/ft_logpos.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0000/00/00 00:00:00 by 5tta              #+#    #+#             */
-/*   Updated: 2014/03/04 22:21:42 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/04 22:28:53 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,30 @@ static void			st_clear_bloc(void)
 	tputs(tgetstr("ce", NULL), 1, ft_putput);
 	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 3), 1, ft_putput);
 	tputs(tgetstr("ce", NULL), 1, ft_putput);
+	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 4), 1, ft_putput);
+	tputs(tgetstr("ce", NULL), 1, ft_putput);
+	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 5), 1, ft_putput);
+	tputs(tgetstr("ce", NULL), 1, ft_putput);
 }
 
 static void			st_log(t_pos *pos)
 {
 	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 0), 1, ft_putput);
-	tputs("| x [", 1, ft_putput);
-	tputs(ft_itoa(pos->x), 1, ft_putput);
-	tputs("]", 1, ft_putput);
+	tputs("| W ", 1, ft_putput);
+	tputs(ft_itoa(CTX->cols), 1, ft_putput);
 	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 1), 1, ft_putput);
-	tputs("| y [", 1, ft_putput);
-	tputs(ft_itoa(pos->y), 1, ft_putput);
-	tputs("]", 1, ft_putput);
+	tputs("| H ", 1, ft_putput);
+	tputs(ft_itoa(CTX->rows), 1, ft_putput);
 	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 2), 1, ft_putput);
-	tputs("| i [", 1, ft_putput);
-	tputs(ft_itoa(CTX->i), 1, ft_putput);
-	tputs("]", 1, ft_putput);
+	tputs("| x ", 1, ft_putput);
+	tputs(ft_itoa(pos->x), 1, ft_putput);
 	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 3), 1, ft_putput);
+	tputs("| y ", 1, ft_putput);
+	tputs(ft_itoa(pos->y), 1, ft_putput);
+	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 4), 1, ft_putput);
+	tputs("| i ", 1, ft_putput);
+	tputs(ft_itoa(CTX->i), 1, ft_putput);
+	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 9, 5), 1, ft_putput);
 	tputs("+--------", 1, ft_putput);
 }
 
