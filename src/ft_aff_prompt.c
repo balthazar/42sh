5ff20166efe0ddf818bc07d2eb72191b170cdf20
mpillet <mpillet@student.42.fr>
2014-03-04 21:03:32 +0100

    [TMP] disabling ps1 temporarly

--- src/ft_aff_prompt.c
+++ src/ft_aff_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0000/00/00 00:00:00 by 5tta              #+#    #+#             */
-/*   Updated: 2014/03/04 16:05:14 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/04 20:58:34 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void				ft_aff_prompt(void)
 	ctx = CTX;
 	if (ctx->prompt)
 		return ;
-	ft_psone(ctx->env);
+/*	ft_psone(ctx->env);*/
 	if (PS->str)
 		ft_putstr(PS->str);
 	else
