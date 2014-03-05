49dada1f9c4d61ce21397ea614f4f3c8bf8698c9
mpillet <mpillet@student.42.fr>
2014-03-05 23:41:46 +0100

    update ft_logpos

--- src/ft_logpos.c
+++ src/ft_logpos.c
@@ -6,84 +6,48 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0000/00/00 00:00:00 by 5tta              #+#    #+#             */
-/*   Updated: 2014/03/05 17:09:01 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/05 22:44:18 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
 
-static void			st_clear_bloc(void)
+static void			st_log(int line, char *name, char *str)
 {
+	char			*tmp;
+
+	tputs(tgoto(tgetstr("cm", NULL), 0, line), 1, ft_putput);
+	tputs(tgetstr("ce", NULL), 1, ft_putput);
+	tmp = ft_strjoin(name, " ");
 	tputs(tgetstr("ei", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 0), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 1), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 2), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 3), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 4), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 5), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 6), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 11, 7), 1, ft_putput);
-	tputs(tgetstr("ce", NULL), 1, ft_putput);
-}
-
-static void			st_log(t_pos *pos, t_ctx *ctx)
-{
-	int				left;
-	int				calc;
-
-	left = ctx->len - ctx->i;
-	calc = ctx->pos.x + left;
-	(void) ctx;
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 0), 1, ft_putput);
-	tputs(" W ", 1, ft_putput);
-	tputs(ft_itoa(CTX->cols), 1, ft_putput);
 	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 1), 1, ft_putput);
-	tputs(" H ", 1, ft_putput);
-	tputs(ft_itoa(CTX->rows), 1, ft_putput);
+	tputs(ft_rpad(tmp,  12, '.'), 1, ft_putput);
 	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 2), 1, ft_putput);
-	tputs(" x ", 1, ft_putput);
-	tputs(ft_itoa(pos->x), 1, ft_putput);
+	tputs("[", 1, ft_putput);
+	tputs(str, 1, ft_putput);
+	tputs("]", 1, ft_putput);
 	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 3), 1, ft_putput);
-	tputs(" y ", 1, ft_putput);
-	tputs(ft_itoa(pos->y), 1, ft_putput);
-	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 4), 1, ft_putput);
-	tputs(" i ", 1, ft_putput);
-	tputs(ft_itoa(CTX->i), 1, ft_putput);
-	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 5), 1, ft_putput);
-	tputs(" L ", 1, ft_putput);
-	tputs(ft_itoa(CTX->len), 1, ft_putput);
-	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 6), 1, ft_putput);
-	tputs("CA ", 1, ft_putput);
-	tputs(ft_itoa(calc), 1, ft_putput);
-	tputs(" ", 1, ft_putput);
-	tputs(tgoto(tgetstr("cm", NULL), CTX->cols - 10, 7), 1, ft_putput);
-	tputs("LE ", 1, ft_putput);
-	tputs(ft_itoa(left), 1, ft_putput);
-	tputs(" ", 1, ft_putput);
+	ft_memdel((void **) &tmp);
 }
 
 void				ft_logpos(void)
 {
-	t_pos			*pos;
+	t_ctx			*ctx;
+	int				i;
 
 	tputs(tgetstr("sc", NULL), 1, ft_putput);
 	tputs(tgetstr("mr", NULL), 1, ft_putput);
-	pos = &CTX->pos;
-	st_clear_bloc();
-	st_log(pos, CTX);
+	ctx = CTX;
+	i = 0;
+
+	st_log(i++, "ctx->cols", ft_itoa(ctx->cols));
+	st_log(i++, "ctx->rows", ft_itoa(ctx->rows));
+	st_log(i++, "ctx->line", ctx->line);
+	st_log(i++, "ctx->len", ft_itoa(ctx->len));
+	st_log(i++, "ctx->i", ft_itoa(ctx->i));
+	st_log(i++, "pos.x", ft_itoa(ctx->pos.x));
+	st_log(i++, "pos.y", ft_itoa(ctx->pos.y));
+
 	tputs(tgetstr("me", NULL), 1, ft_putput);
 	tputs(tgetstr("rc", NULL), 1, ft_putput);
 	tputs(tgetstr("im", NULL), 1, ft_putput);
