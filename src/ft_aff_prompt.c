0eeed581d6d0b78cb93d9c31387058cbbd01e56c
fbeck <fbeck@student.42.fr>
2014-03-07 17:07:45 +0100

    clearing line after aff_prompt

--- src/ft_aff_prompt.c
+++ src/ft_aff_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 0000/00/00 00:00:00 by 5tta              #+#    #+#             */
-/*   Updated: 2014/03/02 23:08:56 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/07 17:06:37 by fbeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void				ft_aff_prompt(void)
 	if (CTX->prompt)
 		return ;
 	ft_putstr("YOLO-Shell> ");
+	tputs(tgetstr("ce", NULL), 1, ft_putput);
 	CTX->prompt = 1;
 }
