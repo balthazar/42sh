22eb113f9274ebe0a08067e2b7784faad5c1c517
mpillet <mpillet@student.42.fr>
2014-03-10 18:25:47 +0100

    working home

--- src/treat_key_home.c
+++ src/treat_key_home.c
@@ -6,7 +6,7 @@
 /*   By: mpillet <mpillet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/10 18:07:15 by mpillet           #+#    #+#             */
-/*   Updated: 2014/03/10 18:10:26 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/10 18:22:30 by mpillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int					treat_key_home(void)
 {
-	printf("PUTE IS PUTE\n");
-	ft_exit(0);
+	t_ctx			*ctx;
+
+	ctx = CTX;
+	while (ctx->i)
+		treat_key_left();
 	return (OK);
 }
