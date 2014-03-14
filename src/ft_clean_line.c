b67bc1c19a7bda68ed58da4136bab8e67a31c9ab
fbeck <fbeck@student.42.fr>
2014-03-14 18:05:23 +0100

    fix clean line

--- src/ft_clean_line.c
+++ src/ft_clean_line.c
@@ -6,7 +6,7 @@
 /*   By: bgronon <bgronon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/04 11:19:58 by bgronon           #+#    #+#             */
-/*   Updated: 2014/03/06 18:50:25 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/03/14 18:03:30 by fbeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_clean_line(void)
 	t_ctx	*ctx;
 
 	ctx = CTX;
+	tputs(tgetstr("cd", NULL), 1, ft_putput);
 	while (ctx->i > 0)
 	{
 		if (ctx->pos.x == 0)
